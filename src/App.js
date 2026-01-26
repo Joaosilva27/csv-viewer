7bc58848041d86ac6a0d80319ba1745056057518
Joaosilva27 <joaosilva7875@gmail.com>
2026-01-26 18:55:32 +0000

    cors policy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
-import conectys from './conectys.png';
+import conectys from "./conectys.png";
 
 /* ===================== ICONS ===================== */
 
@@ -170,7 +170,7 @@ function App() {
     <div className={`app-container ${darkMode ? "dark" : ""}`}>
       <nav className='navbar'>
         <div className='nav-brand'>
-          <img src={conectys}ys alt='Conectys Logo' style={{ width: 130 }} />
+          <img src={conectys} alt='Conectys Logo' style={{ width: 130 }} />
         </div>
 
         <button className='btn-icon' onClick={() => setDarkMode(!darkMode)} aria-label='Toggle theme'>
